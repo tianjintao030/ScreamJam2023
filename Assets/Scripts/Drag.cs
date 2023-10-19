5c16d5961a6a6d97745c399d8f41a00d12e70181
tianjintao030 <85120907+tianjintao030@users.noreply.github.com>
2023-10-19 23:19:04 +0800

    10.19 1

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
-    public Transform correct_trans;
-    private Vector2 start_pos;
-    private bool is_finshed;
+    public Transform correct_trans;//应该把它正确放置的transform
+    private Vector2 start_pos;//初始时的位置
+    public bool is_selected;//是否被选中
+    public bool is_finshed;//是否已经完成放置
+    public bool is_floation;//是否是漂浮物
+    public Vector2 floatiion_offsetXY;//漂浮的范围
+    public float floation_amplitude;//漂浮的幅度
+    public float Timer = 0;//漂浮效果的计时器
+    public float floation_move_interval;//漂浮时的随机变化时间间隔
+
     void Start()
     {
         start_pos = transform.position;
@@ -14,16 +21,20 @@ public class Drag : MonoBehaviour
 
     void Update()
     {
-
+        Timer += Time.deltaTime;
+        Floation();
     }
 
     private void OnMouseDrag()
     {
+        is_selected = true;
         FollowMouse();
     }
 
     private void OnMouseUp()
     {
+        is_selected = false;
+
         //该对象位置与正确放置位置相近则完成
         if(Mathf.Abs(correct_trans.position.x-transform.position.x)<0.5f &&
             Mathf.Abs(correct_trans.position.y-transform.position.y)<0.5f)
@@ -53,5 +64,46 @@ public class Drag : MonoBehaviour
         }
     }
 
+    private void Floation()
+    {
+        if(is_floation && !is_selected && Timer>=floation_move_interval)
+        {
+            int x;
+            x = Random.Range(0, 4);
+
+            if(x==0)
+            {
+                transform.position = start_pos + new Vector2(
+                    floatiion_offsetXY.x * Mathf.Sin(Time.time * floation_amplitude),
+                    floatiion_offsetXY.y * Mathf.Sin(Time.time * floation_amplitude));
+
+                Timer = 0;
+            }
+            if(x==1)
+            {
+                transform.position = start_pos + new Vector2(
+                    floatiion_offsetXY.x * Mathf.Cos(Time.time * floation_amplitude),
+                    floatiion_offsetXY.y * Mathf.Sin(Time.time * floation_amplitude));
+
+                Timer = 0;
+            }
+            if(x==2)
+            {
+                transform.position = start_pos + new Vector2(
+                    floatiion_offsetXY.x * Mathf.Sin(Time.time * floation_amplitude),
+                    floatiion_offsetXY.y * Mathf.Cos(Time.time * floation_amplitude));
+
+                Timer = 0;
+            }
+            if(x==3)
+            {
+                transform.position = start_pos + new Vector2(
+                    floatiion_offsetXY.x * Mathf.Cos(Time.time * floation_amplitude),
+                    floatiion_offsetXY.y * Mathf.Cos(Time.time * floation_amplitude));
+
+                Timer = 0;
+            }
+        }
+    }
    
 }
